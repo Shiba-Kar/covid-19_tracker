3f00254214a7f41c00ac13f00c86bc6bce090a64
Shiba Prasad Kar <shibaprasadkar50@gmail.com>
2020-05-06 23:22:13 +0530

    drawer added

---
+++ lib/widgets/CustomDrawer.dart
@@ -0,0 +1,176 @@
+import 'dart:math' as math;
+
+import 'package:covid_19/widgets/Nm_box.dart';
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatefulWidget {
+  final Widget child;
+
+  const CustomDrawer({Key key, this.child}) : super(key: key);
+
+  static CustomDrawerState of(BuildContext context) =>
+      context.findAncestorStateOfType<CustomDrawerState>();
+
+  @override
+  CustomDrawerState createState() => new CustomDrawerState();
+}
+
+class CustomDrawerState extends State<CustomDrawer>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  bool _canBeDragged = false;
+  final double maxSlide = 300.0;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  void toggle() => animationController.isDismissed
+      ? animationController.forward()
+      : animationController.reverse();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: _onDragStart,
+      onHorizontalDragUpdate: _onDragUpdate,
+      onHorizontalDragEnd: _onDragEnd,
+      behavior: HitTestBehavior.translucent,
+      onTap: toggle,
+      child: AnimatedBuilder(
+        animation: animationController,
+        builder: (context, _) {
+          return Material(
+            color: mCD,
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(maxSlide * (animationController.value - 1), 0),
+                  child: Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(math.pi / 2 * (1 - animationController.value)),
+                    alignment: Alignment.centerRight,
+                    child: MyDrawer(),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(maxSlide * animationController.value, 0),
+                  child: Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(-math.pi * animationController.value / 2),
+                    alignment: Alignment.centerLeft,
+                    child: widget.child,
+                  ),
+                ),
+                Positioned(
+                  top: 4.0 + MediaQuery.of(context).padding.top,
+                  left: 4.0 + animationController.value * maxSlide,
+                  child: IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: toggle,
+                    color: Colors.white,
+                  ),
+                ),
+               
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _onDragStart(DragStartDetails details) {
+    bool isDragOpenFromLeft = animationController.isDismissed;
+    bool isDragCloseFromRight = animationController.isCompleted;
+    _canBeDragged = isDragOpenFromLeft || isDragCloseFromRight;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    if (_canBeDragged) {
+      double delta = details.primaryDelta / maxSlide;
+      animationController.value += delta;
+    }
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    //I have no idea what it means, copied from Drawer
+    double _kMinFlingVelocity = 365.0;
+
+    if (animationController.isDismissed || animationController.isCompleted) {
+      return;
+    }
+    if (details.velocity.pixelsPerSecond.dx.abs() >= _kMinFlingVelocity) {
+      double visualVelocity = details.velocity.pixelsPerSecond.dx /
+          MediaQuery.of(context).size.width;
+
+      animationController.fling(velocity: visualVelocity);
+    } else if (animationController.value < 0.5) {
+      animationController.reverse();
+    } else {
+      animationController.forward();
+    }
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      height: double.infinity,
+      child: Material(
+        color: mC,
+        child: SafeArea(
+          child: Theme(
+            data: ThemeData(brightness: Brightness.dark),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                 Image.asset(
+            
+                  'assets/images/drawer.png',
+                  fit: BoxFit.fill,
+                ), 
+                ListTile(
+                  leading: Icon(Icons.new_releases),
+                  title: Text('News'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.star),
+                  title: Text('Favourites'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.map),
+                  title: Text('Map'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Profile'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
