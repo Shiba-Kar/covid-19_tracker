fb7b4387875ff4d13fdd1fbf96e5f8da87b9dc5a
Shiba Prasad Kar <shibaprasadkar50@gmail.com>
2020-05-09 22:49:05 +0530

    improved data presenting

--- lib/widgets/StateCard.dart
+++ lib/widgets/StateCard.dart
@@ -1,19 +1,18 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:covid_19/models/CovidDataIndianModel.dart';
-import 'package:covid_19/models/StateDistrictCovidDataModel.dart';
-import 'package:covid_19/screens/DetailedCountryScreen.dart';
-import 'package:covid_19/screens/DetailedStateScreen.dart';
-import 'package:covid_19/screens/IndianStatesCovidPage.dart';
-import 'package:covid_19/widgets/GlowFlags.dart';
+import 'package:covid_19/screens/DistrictsScreen.dart';
 import 'package:covid_19/widgets/Nm_box.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class StateCard extends StatefulWidget {
-  final Statewise stateDistrictCovidDataModel;
+  final Statewise stateWise;
   final int index;
+  final bool tapable;
   const StateCard(
-      {@required this.stateDistrictCovidDataModel,
+      {@required this.stateWise,
       @required this.index,
+      @required this.tapable,
       Key key})
       : super(key: key);
 
@@ -29,7 +28,7 @@ class _StateCardState extends State<StateCard>
   void initState() {
     controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 400));
-    scaleAnimation  = Tween<Offset>(begin: Offset(2.0, 0.0), end: Offset.zero)
+    scaleAnimation = Tween<Offset>(begin: Offset(2.0, 0.0), end: Offset.zero)
         .animate(controller);
 
     controller.addListener(() {
@@ -50,9 +49,24 @@ class _StateCardState extends State<StateCard>
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
+    Widget text(String text, Color color, FontWeight fontWeight) {
+      return Text(
+        text,
+        style: TextStyle(color: color, fontSize: 13.0, fontWeight: fontWeight),
+      );
+    }
+
+    Widget updatedTime() {
+      var x = DateTime.parse(widget.stateWise.lastupdatedtime);
+      var formattedDate = DateFormat.yMMMd().format(x);
+
+      return Text(
+        "Updated on " + formattedDate,
+        style: TextStyle(color: mCL),
+      );
+    }
 
     return SlideTransition(
-
       position: scaleAnimation,
       child: Container(
         margin: const EdgeInsets.all(10.0),
@@ -66,12 +80,119 @@ class _StateCardState extends State<StateCard>
                 child: ListTile(
                   onTap: () => Navigator.of(context).push(
                     MaterialPageRoute(
-                        builder: (context) => null)
+                      builder: (context) => DistrictsScreen(
+                          stateCode: widget.stateWise.statecode,
+                          state: widget.stateWise.state),
+                    ),
                   ),
                   contentPadding: const EdgeInsets.all(10.0),
                   title: Text(
-                    widget.stateDistrictCovidDataModel.state,
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    widget.stateWise.state,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  text(
+                                    "CASES : ",
+                                    Colors.yellow,
+                                    FontWeight.bold,
+                                  ),
+                                  text(
+                                    widget.stateWise.confirmed.toString(),
+                                    Colors.yellow,
+                                    FontWeight.normal,
+                                  ),
+                                ],
+                              ),
+                              text(
+                                "+" +
+                                    widget.stateWise.deltaconfirmed.toString(),
+                                Colors.yellow,
+                                FontWeight.bold,
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              text(
+                                "ACTIVE : ",
+                                Colors.blue,
+                                FontWeight.bold,
+                              ),
+                              text(
+                                widget.stateWise.active.toString(),
+                                Colors.blue,
+                                FontWeight.normal,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Divider(
+                        // indent: 50.0,
+                        color: Colors.white,
+                        // endIndent: 50.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              text(
+                                "RECOVERED : ",
+                                Colors.green,
+                                FontWeight.bold,
+                              ),
+                              text(
+                                widget.stateWise.confirmed.toString(),
+                                Colors.green,
+                                FontWeight.normal,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: <Widget>[
+                                  text(
+                                    "DEATH : ",
+                                    Colors.red,
+                                    FontWeight.bold,
+                                  ),
+                                  text(
+                                    widget.stateWise.deaths.toString(),
+                                    Colors.red,
+                                    FontWeight.normal,
+                                  ),
+                                ],
+                              ),
+                              text(
+                                "+" + widget.stateWise.deltadeaths.toString(),
+                                Colors.red,
+                                FontWeight.bold,
+                              )
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
