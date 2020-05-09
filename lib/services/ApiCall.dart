ee35c9560ec6ea77471f98e155534a02f94cf625
Shiba Prasad Kar <shibaprasadkar50@gmail.com>
2020-05-09 22:50:45 +0530

    fuction added to get districts from statecode

--- lib/services/ApiCall.dart
+++ lib/services/ApiCall.dart
@@ -8,9 +8,6 @@ import 'package:covid_19/models/CovidDataAllModel.dart';
 import 'package:covid_19/models/CovidDataCountriesModel.dart';
 import 'package:covid_19/models/CovidDataIndianModel.dart';
 import 'package:covid_19/models/StateDistrictCovidDataModel.dart';
-
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
 
 class ApiCall {
@@ -37,27 +34,6 @@ class ApiCall {
     }
   }
 
-  /*void streamGetFilteredCountries({String filter}) async* {
-    String path = ((await getTemporaryDirectory()).path);
-
-    String _urlCovidCountries = filter != null
-        ? "https://corona.lmao.ninja/v2/countries?sort=$filter"
-        : "https://corona.lmao.ninja/v2/countries?sort=cases";
-
-    try {
-      final myDataResource = HttpNetworkResource<List<CovidDataCountriesModel>>(
-        url: _urlCovidCountries,
-        parser: (contents) => covidDataCountriesModelFromJson(contents),
-        cache: FileResource(File('$path/$filter.json')),
-        maxAge: Duration(minutes: 8),
-        strategy: CacheStrategy.cacheFirst,
-      );
-    } catch (e) {
-      print(e);
-      yield* null;
-    }
-  }*/
-
   Future<CovidDataIndianModel> getIndianData() async {
     String path = ((await getTemporaryDirectory()).path);
 
@@ -78,29 +54,41 @@ class ApiCall {
     }
   }
 
-  
-  /* Future<List<StateDistrictCovidDataModel>> getIndianStateDistrict() async {
+  Future<List<DistrictDatum>> getDistricts(String stateCode) async {
     String path = ((await getTemporaryDirectory()).path);
 
-    String _urlIndian = "https://api.covid19india.org/v2/state_district_wise.json";
+    String _urlIndian =
+        "https://api.covid19india.org/v2/state_district_wise.json";
+
+    List<DistrictDatum> filteredList = [];
 
     try {
-      final myDataResource = HttpNetworkResource<List<StateDistrictCovidDataModel>>(
+      final myDataResource =
+          HttpNetworkResource<List<StateDistrictCovidDataModel>>(
         url: _urlIndian,
         parser: (contents) => stateDistrictCovidDataModelFromJson(contents),
         cache: FileResource(File('$path/state_district.json')),
         maxAge: Duration(minutes: 8),
         strategy: CacheStrategy.cacheFirst,
       );
-      return myDataResource.get();
+      var x = await myDataResource.get();
+
+      var y = x
+          .where((element) => element.statecode.startsWith(stateCode))
+          .toList();
+      for (var item in y[0].districtData) {
+        filteredList.add(item);
+      }
+
+      return filteredList;
     } catch (e) {
       print(e);
       return null;
     }
-  } */
+  }
 
   Future<CovidDataAllModel> getCovidDataGolobal() async {
-    String _urlCovidAll = "https://corona.lmao.ninja/v2/all";
+    String _urlCovidAll = "https://corona.lmao.ninja/v2/all?yesterday=false";
     String path = ((await getTemporaryDirectory()).path);
     try {
       final myDataResource = HttpNetworkResource<CovidDataAllModel>(
@@ -117,15 +105,17 @@ class ApiCall {
     }
   }
 
-  Future<CovidCountyHistoryModel> getHistoricalData({String country,int pastRecords}) async {
-    String url = "https://corona.lmao.ninja/v2/historical/$country?lastdays=$pastRecords";
+  Future<CovidCountyHistoryModel> getHistoricalData(
+      {String country, int pastRecords}) async {
+    String url =
+        "https://corona.lmao.ninja/v2/historical/$country?lastdays=$pastRecords";
     String path = ((await getTemporaryDirectory()).path);
     try {
       final myDataResource = HttpNetworkResource<CovidCountyHistoryModel>(
         url: url,
         parser: (contents) => covidCountyHistoryModelFromJson(contents),
         cache: FileResource(File('$path/history_$country.json')),
-        maxAge: Duration(seconds: 1),
+        maxAge: Duration(minutes: 8),
         strategy: CacheStrategy.networkFirst,
       );
       return myDataResource.get();
