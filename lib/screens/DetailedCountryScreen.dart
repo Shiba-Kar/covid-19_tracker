9af1df43c896449ad8729f6061d7d8f9c68b411f
Shiba Prasad Kar <shibaprasadkar50@gmail.com>
2020-05-10 22:17:24 +0530

    added gridCard for showing more informations

--- lib/screens/DetailedCountryScreen.dart
+++ lib/screens/DetailedCountryScreen.dart
@@ -3,6 +3,7 @@ import 'package:covid_19/models/CovidDataCountriesModel.dart';
 import 'package:covid_19/services/ApiCall.dart';
 import 'package:covid_19/widgets/CountryCard.dart';
 import 'package:covid_19/widgets/CountryHistoryStatistics.dart';
+import 'package:covid_19/widgets/CustomGridCard.dart';
 import 'package:covid_19/widgets/LoadingIndicator.dart';
 import 'package:covid_19/widgets/Nm_box.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +25,8 @@ class _DetailedCountryScreenState extends State<DetailedCountryScreen> {
   @override
   Widget build(BuildContext context) {
     final ApiCall _apiCall = ApiCall();
+    final countryName=widget.covidDataCountriesModel.country;
+    
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
@@ -34,18 +37,85 @@ class _DetailedCountryScreenState extends State<DetailedCountryScreen> {
       appBar: AppBar(
         
         elevation: 0.0,
-        title: Text(widget.covidDataCountriesModel.country),
+        title: Text(countryName),
         backgroundColor: mC,
       ),
       body: ListView(
         children: <Widget>[
           CountryCard(
+            tapable: false,
             covidDataCountriesModel: widget.covidDataCountriesModel,
           ),
+           Container(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "More Information",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
           Container(
+                padding: const EdgeInsets.all(0.0),
+                height: height / 2,
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 2,
+                    crossAxisSpacing: 20.0,
+                    mainAxisSpacing: 20.0,
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  children: <Widget>[
+                    CustomGridCard(
+                      title: "Cases/1M",
+                      end: widget.covidDataCountriesModel.casesPerOneMillion.toDouble(),
+                      color: Colors.yellow,
+                      showChange: false,
+                      
+                    ),
+                    CustomGridCard(
+                      title: "Deaths/1M",
+                      end: widget.covidDataCountriesModel.deathsPerOneMillion.toDouble(),
+                      color: Colors.redAccent,
+                      showChange: false,
+                    ),
+                   CustomGridCard(
+                      title: "Critical",
+                      end: widget.covidDataCountriesModel.critical.toDouble(),
+                      color: Colors.orange,
+                      showChange: false,
+                    ),
+                    CustomGridCard(
+                      title: "Tests",
+                      end: widget.covidDataCountriesModel.tests.toDouble(),
+                      color: Colors.green,
+                      showChange: false,
+                    ),
+                    CustomGridCard(
+                      title: "Tests/1M",
+                      end: widget.covidDataCountriesModel.testsPerOneMillion.toDouble(),
+                      color: Colors.blue,
+                      showChange: false,
+                    ),
+                     
+                  ],
+                ),
+              ),
+           Container(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "$countryName History",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),
+              ),
+            ),
+          Container(
+            decoration: nMbox,
+            margin: const EdgeInsets.all(30.0),
             height: height / 1.6,
-            child: CountryHistoryStatistics(
-              country: widget.covidDataCountriesModel.country,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CountryHistoryStatistics(
+                country: widget.covidDataCountriesModel.country,
+              ),
             ),
           ),
         ],
